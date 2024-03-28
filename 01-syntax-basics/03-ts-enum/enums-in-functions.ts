3f61c3df48ac9691b308c857b046ba3b1a3eb219
Matvey Eikholts <matze85eikholts@gmail.com>
2024-03-28 16:38:25 +0300

    After long break some coding

--- 01-syntax-basics/03-ts-enum/enums-in-functions.ts
+++ 01-syntax-basics/03-ts-enum/enums-in-functions.ts
@@ -7,3 +7,9 @@ function readStatus (application: AppStatus): void {
   console.log(application);
 }
 readStatus(AppStatus.ACTIVE);
+
+
+const outerFunc = (someValue: number) => (multiplier: number) => someValue * multiplier;
+const innerFunc = outerFunc(10);
+let result = innerFunc(5);
+console.log(result);
